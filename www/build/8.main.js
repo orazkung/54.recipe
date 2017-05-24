a88122b236733b2bd00caef5c3332c0acc41e438
orazkung <orazkung@gmail.com>
2017-05-24 11:47:45 +0700

    Add files via upload

---
+++ www/build/8.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([8],{
+
+/***/ 274:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__dessert__ = __webpack_require__(48);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DessertPageModule", function() { return DessertPageModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var DessertPageModule = (function () {
+    function DessertPageModule() {
+    }
+    return DessertPageModule;
+}());
+DessertPageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__dessert__["a" /* DessertPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__dessert__["a" /* DessertPage */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__dessert__["a" /* DessertPage */]
+        ]
+    })
+], DessertPageModule);
+
+//# sourceMappingURL=dessert.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=8.main.js.map
